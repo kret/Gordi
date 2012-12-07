22adad983f8ebfe63c7a09da6ddf18b8e314ad79
Andrzej Undziłło <s4694@pjwstk.edu.pl>
2012-12-07 21:07:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gordi::Application.config.secret_token = 'a05d589b13190070db00c008c9d132fcee17f0bd49c0f3b82a6df291c36357083eddffdd04db1797f2ac549b23e47bbcad5333ee0cc5bb5077ab934ccf987118'
