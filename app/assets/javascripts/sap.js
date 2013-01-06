b792e273295d88efe9f9febb88bc3d1c0cc80c1f
Andrzej Undziłło <s4694@pjwstk.edu.pl>
2013-01-06 12:44:39 +0100

    Implement drawPoints method on SAP

--- app/assets/javascripts/sap.js
+++ app/assets/javascripts/sap.js
@@ -20,11 +20,15 @@ var util = {
 		var start = null;
 		var current = null;
 		var x = 0, y = 0;
+		var groupsList = [];
 
 		function SAP(parent, _parameters) {
 			this.parameters = _parameters;
 			this.$el = $('<svg xmlns="http://www.w3.org/2000/svg" id="svg' + (new Date()).getTime() + '" width="400px" height="400px">');
 			parent.append(this.$el);
+		}
+
+		SAP.prototype.actsAsTrainingSet = function() {
 			this.$el.on('click', function(evt) {
 				if (!second) {
 					var dotr = 6;
@@ -59,12 +63,28 @@ var util = {
 					var ty = evt.offsetY;
 					var E = document.getElementById(current);
 					E.setAttributeNS(null, 'r', Math.ceil(Math.sqrt(Math.pow(tx - x, 2) + Math.pow(ty - y, 2))));
+					groupsList.push(E);
 				}
 			});
-		}
+		};
 
-		SAP.prototype.addPoints = function(_points) {
+		SAP.prototype.drawPoints = function(_points) {
 			this.points = _points;
+			var el = null;
+			var pointData = null;
+			for (var i = 0; i < this.points.length; i += 1) {
+				pointData = this.points[i];
+				for (var j = 0; j < pointData.points.length; j += 1) {
+					el = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+					el.setAttributeNS(null, 'cx', pointData.points[j][0]);
+					el.setAttributeNS(null, 'cy', pointData.points[j][0]);
+					el.setAttributeNS(null, 'r', 3);
+					el.setAttributeNS(null, 'fill', pointData.color);
+					el.setAttributeNS(null, 'stroke', '#000000');
+					el.setAttributeNS(null, 'stroke-width', 2);
+					this.$el.append(el);
+				}
+			}
 		};
 
 		return SAP;
@@ -72,9 +92,11 @@ var util = {
 }(jQuery));
 
 var training_set_board = new util.drawing.SAP(jQuery('#training_set'), {});
+training_set_board.actsAsTrainingSet();
+
 /*
-training_set_board.addPoints([
-	{ color: '#eeffaa', points: [[2,4], [7,8], [12,1]] },
-	{ color: '#aadd33', points: [[5,6], [1,2], [4,6]] }
+training_set_board.drawPoints([
+	{ color: '#eeffaa', points: [[2,4], [17,18], [22,21]] },
+	{ color: '#aadd33', points: [[35,36], [41,42], [54,56]] }
 ]);
 */
