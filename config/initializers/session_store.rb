38853ea7aaa1a8039570d276c52f79f29f00ac35
Andrzej Undziłło <s4694@pjwstk.edu.pl>
2013-01-07 23:46:45 +0100

    Add active record session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Gordi::Application.config.session_store :cookie_store, key: '_Gordi_session'
+# Gordi::Application.config.session_store :cookie_store, key: '_Gordi_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Gordi::Application.config.session_store :active_record_store
+Gordi::Application.config.session_store :active_record_store
